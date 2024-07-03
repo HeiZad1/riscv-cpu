4ea3b6b5955054866fcb26b2914c125fd744c5f9
123456 Song wenfei <songwenfei999@gmail.com>
2024-07-03 10:58:39 +0800

    v4

--- include/RV32I_defines.v
+++ include/RV32I_defines.v
@@ -14,8 +14,12 @@
 `define RFIDX_WIDTH   5
 `define RFREG_NUM_IS_32 
 `define RFREG_NUM     32
+`define XLEN_VEC     128
+`define PUREG_NUM     4
 
 //ff
 `define NOP   		32'H00000013 
 `define RESET_PC    32'H80000000 
+
+
 
